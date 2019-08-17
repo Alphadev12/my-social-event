7033e85a18fbf403085862a6d070885639d53e69
Alpha <ravalasonn@gmail.com>
2019-08-17 06:34:08 +0300

    Validation Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,37 @@
 class Event < ApplicationRecord
+
+
+	validates :start_date,
+	presence: true
+	validate :validate_start_date?
+
+	validates :duration,
+	presence: true
+	validate :validate_duration?
+
+	validates :title,
+	presence: true,
+	length: { in: 5..140 }
+
+	validates :description,
+	presence: true,
+	length: { in: 20..1000 }
+
+	validates :price,
+	presence: true
+
+	validates :location,
+	presence: true
+
+	def validate_start_date?
+		if start_date < Time.now
+			errors.add(:start_date, "La date ne doit pas être une date antérieur")
+		end
+	end
+
+	def validate_duration?
+		unless duration > 0 && duration % 5 == 0
+			errors.add(:duration, "La durer doit être un multiple de 5")
+		end
+	end
 end
