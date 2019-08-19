0c37179e1e505e0fc91357c9bc03b0d014564be1
Alpha <ravalasonn@gmail.com>
2019-08-19 16:24:26 +0300

    Ajout d'un évenement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,30 @@ class EventsController < ApplicationController
   end
 
   def create
+    @start_date = params[:start_date]
+    @duration = params[:duration]
+    @title = params[:title]
+    @description = params[:description]
+    @price = params[:price]
+    @location = params[:location]
+
+    @event = Event.new(
+      start_date: @start_date,
+      duration: @duration,
+      title: @title,
+      description: @description,
+      price: @price,
+      location: @location
+    )
+
+    @event.admin = current_user
+
+    if @event.save
+      flash[:success] = "L'évenement a été bien ajouter"
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def edit
