4589eae8547649b73ddb971337403177f8f36a6d
Alpha <ravalasonn@gmail.com>
2019-08-21 07:30:12 +0300

    Contrôle participation

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,4 +1,7 @@
 class ParticipationsController < ApplicationController
+	before_action :authenticate_user!, only: [:new]
+
+
 	def index
 		event_id = params[:event_id]
 		@event = Event.find(event_id)
